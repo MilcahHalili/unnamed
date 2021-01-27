9c8edeb1bc0dd783cce06a95ea29ea63e7e86567
Milcah <milcah@macbook-air.lan>
2021-01-26 16:17:14 -0800

    add code to empty files, update favicon

--- pages/index.js
+++ pages/index.js
@@ -6,8 +6,8 @@ export default function Home() {
   return (
     <div className={styles.container}>
       <Head>
-        <title>Create Next App</title>
-        <link rel="icon" href="/favicon.ico" />
+        <title>unnamed</title>
+        <link rel="icon" type="image/png" href="/favicon.ico" />
       </Head>
       
       <nav>
